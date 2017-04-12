c1b71304f06b64cf2a75b49fdecf0ea7d40f1356
pippisargent <pippi.sargent@gmail.com>
2017-04-12 12:20:20 +1200

    Project stuff dont need yet

--- views/Projects/Projects.js
+++ views/Projects/Projects.js
@@ -2,7 +2,8 @@ function ProjectsController(ProjectService, $scope) {
   this.projects = [];
 
   this.$onInit = () => {
-    //need to
-    ProjectService.getProjectsForBuilding( ).then((projects) => this.projects = data.projects).then(() => $scope.$applyAsync());
+    ProjectService.getProjects().then((projects) => this.projects = data.projects).then(() => $scope.$applyAsync());
   }
+
+  //this.logout
 }
