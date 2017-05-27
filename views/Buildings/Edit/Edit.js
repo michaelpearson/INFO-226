b2fb47ebad348e0d77cb2904c0738ab9248ed26e
Michael <zl2mjp@gmail.com>
2017-05-27 15:45:54 +1200

    Restrict editing building owner to owners

--- views/Buildings/Edit/Edit.js
+++ views/Buildings/Edit/Edit.js
@@ -1,6 +1,6 @@
 function BuildingEditController(building, $state, BuildingService, users) {
   this.building = building;
-  this.users = users;
+  this.users = users.filter(u => u.UserType === OWNER);
   this.isNew = building.ID === 'new';
 
   this.save = () => {
